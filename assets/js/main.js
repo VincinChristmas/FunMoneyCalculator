d6dcd427e44fe91dad424851b6c2f38010c5a7fd
VincinChristmas <VincinChristmas@hotmail.com>
2024-02-28 07:17:44 +0800

    made some js changes today

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,38 @@
-let total = 0;
+function Calculator {
+  'zero': document.getElementById('zero');
+  'one': document.getElementById('one');
+  'two': document.getElementById('two');
+  'three': document.getElementById('three');
+  'four': document.getElementById('four');
+  'five': document.getElementById('five');
+  'six': document.getElementById('six');
+  'seven': document.getElementById('seven');
+  'eight': document.getElementById('eight');
+  'nine': document.getElementById('nine');
+  'dotSign': document.getElementById('dotSign');
+  'equalSign': document.getElementById('equalSign');
+  'plusSign': document.getElementById('plusSign');
+  'minusSign': document.getElementById('minusSign');
+  'multiplySign': document.getElementById('multiplySign');
+  'divideSign': document.getElementById('divideSign');
+
+  addition(num) {
+    let total = 0;
+    return total + num;
+    document.querySelector('#theResults').innerText = total;
+  }
+
+  
+
+}
+
+
+
+
+
+
+/*let total = 0;
+let num = 0;
 
 document.querySelector('#zero').addEventListener('click', makeZero);
 document.querySelector('#one').addEventListener('click', makeOne);
@@ -10,56 +44,60 @@ document.querySelector('#six').addEventListener('click', makeSix);
 document.querySelector('#seven').addEventListener('click', makeSeven);
 document.querySelector('#eight').addEventListener('click', makeEight);
 document.querySelector('#nine').addEventListener('click', makeNine);
+document.querySelector('#plusSign').addEventListener('click', Add)
 
+function Add(num) {
+ return total + num;
+}
 
 function makeZero() {
-  total = 0;
-  document.querySelector('#theResults').innerText = total;
+  num = num + 0;
+  document.querySelector('#theResults').innerText = num;
 }
 
 function makeOne() {
-  total = total + 1;
-  document.querySelector('#theResults').innerText = total;
+  num = num + 1;
+  document.querySelector('#theResults').innerText = num;
 }
 
 function makeTwo() {
-  total = total + 2;
-  document.querySelector('#theResults').innerText = total;
+  num = num + 2;
+  document.querySelector('#theResults').innerText = num;
 }
 
 function makeThree() {
-  total = total + 3;
-  document.querySelector('#theResults').innerText = total;
+  num = num + 3;
+  document.querySelector('#theResults').innerText = num;
 }
 
 function makeFour() {
-  total = total + 4;
-  document.querySelector('#theResults').innerText = total;
+  num = num + 4;
+  document.querySelector('#theResults').innerText = num;
 }
 
 function makeFive() {
-  total = total + 5;
-  document.querySelector('#theResults').innerText = total;
+  num = num + 5;
+  document.querySelector('#theResults').innerText = num;
 }
 
 function makeSix() {
-  total = total + 6;
-  document.querySelector('#theResults').innerText = total
+  num = num + 6;
+  document.querySelector('#theResults').innerText = num;
 }
 
 function makeSeven(){
-  total = total + 7;
-  document.querySelector('#theResults').innerText = total
+  num = num + 7;
+  document.querySelector('#theResults').innerText = num;
 }
 
 function makeEight() {
-  total = total + 8;
-  document.querySelector('#theResults').innerText = total
+  num = num + 8;
+  document.querySelector('#theResults').innerText = num;
 }
 
 function makeNine() {
-  total = total + 9;
-  document.querySelector('#theResults').innerText = total
+  num = num + 9;
+  document.querySelector('#theResults').innerText = num
 }
 
 /*
