11fe2c6996c7bd3c6e808084161815683f3641ae
VincinChristmas <VincinChristmas@hotmail.com>
2024-03-02 18:54:35 +0800

    completed very basic math calculation functions for this calculator

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,7 @@
-let display = '';
+
+
+let display='';
+let total = [];
 
 document.querySelector('#seven').addEventListener('click', makeSeven);
 document.querySelector('#four').addEventListener('click', makeFour);
@@ -19,86 +22,149 @@ document.querySelector('#minusSign').addEventListener('click', makeMinusSign);
 
 
 
-function makeSeven() {
+
+
+
+function makeSeven ()  {
   display = display + 7;
   document.querySelector('#theResults').innerText = display;
+  total.push(7)
 }
 
 function makeFour() {
   display = display + 4;
   document.querySelector('#theResults').innerText = display;
+  total.push(4)
 }
 
 function makeOne() {
   display = display + 1;
   document.querySelector('#theResults').innerText = display;
+  total.push(1)
 }
 
 function makeZero() {
   display = display + 0;
   document.querySelector('#theResults').innerText = display;
+  total.push(0)
 }
 
 function makeEight() {
   display = display + 8;
   document.querySelector('#theResults').innerText = display;
+  total.push(8)
 }
 
 function makeFive() {
   display = display + 5;
   document.querySelector('#theResults').innerText = display;
+  total.push(5)
 }
 
 function makeTwo() {
   display = display + 2;
   document.querySelector('#theResults').innerText = display;
+  total.push(2)
 }
 
 function makeDot() {
   display = display + '.';
   document.querySelector('#theResults').textContent = display;
+  total.push('.')
 }
 
 function makeNine() {
   display = display + 9;
   document.querySelector('#theResults').innerText = display;
+  total.push(9)
 }
 
 function makeSix() {
   display = display + 6;
   document.querySelector('#theResults').innerText = display;
+  total.push(6)
 }
 
 function makeThree() {
   display = display + 3;
   document.querySelector('#theResults').innerText = display;
+  total.push(3)
 }
 
-function makeEqual() {
-  display = display + '=';
-  document.querySelector('#theResults').innerText = display;
-}
+
 
 function makeDivide() {
   display = display + '÷';
   document.querySelector('#theResults').innerText = display;
+  total.push('÷')
 }
 
 
 function makeMultiply() {
   display = display + '×';
   document.querySelector('#theResults').innerText = display;
+  total.push('×')
 }
 
 function makePlusSign() {
   display = display + '+';
   document.querySelector('#theResults').innerText = display;
+  total.push('+')
 }
 
 function makeMinusSign() {
-  display = display + '-';
-  document.querySelector('#theResults').innerText = display;
+  display = display + '-'
+  document.querySelector('#theResults').innerText = display
+  total.push('-')
+  
 }
+
+function makeEqual() {
+   display = total
+   let capStringOne = ''
+   let capStringTwo = ''
+
+   if (display.includes('-')) {
+   for (let i = 0; i <= display.length; i++) {
+   capStringOne = Number(display.slice(0, display.indexOf('-')).join(""))
+   capStringTwo = Number(display.slice(display.indexOf('-') + 1).join(""))
+   }
+   document.querySelector('#theResults').innerText = capStringOne - capStringTwo;
+  }
+
+  if (display.includes('÷')) {
+    for (let i = 0; i <= display.length; i++) {
+    capStringOne = Number(display.slice(0, display.indexOf('÷')).join(""))
+    capStringTwo = Number(display.slice(display.indexOf('÷') + 1).join(""))
+    }
+    document.querySelector('#theResults').innerText = capStringOne / capStringTwo;
+   }
+   
+   if (display.includes('×')) {
+    for (let i = 0; i <= display.length; i++) {
+    capStringOne = Number(display.slice(0, display.indexOf('×')).join(""))
+    capStringTwo = Number(display.slice(display.indexOf('×') + 1).join(""))
+    }
+    document.querySelector('#theResults').innerText = capStringOne * capStringTwo;
+   }
+
+   if (display.includes('+')) {
+    for (let i = 0; i <= display.length; i++) {
+    capStringOne = Number(display.slice(0, display.indexOf('+')).join(""))
+    capStringTwo = Number(display.slice(display.indexOf('+') + 1).join(""))
+    }
+    document.querySelector('#theResults').innerText = capStringOne + capStringTwo;
+   }
+
+  }
+    
+  
+  
+
+
+
+  
+  
 
 
 
