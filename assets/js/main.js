4b1b023bde197d7b905eee2ea33f3d31d5091f0e
VincinChristmas <VincinChristmas@hotmail.com>
2024-03-09 09:01:01 +0800

    changed the javascript into Class object oriented format

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
-
-
-let display='';
+/*let display='';
 let total = [];
+
 
 document.querySelector('#seven').addEventListener('click', makeSeven);
 document.querySelector('#four').addEventListener('click', makeFour);
@@ -156,9 +155,146 @@ function makeEqual() {
     document.querySelector('#theResults').innerText = capStringOne + capStringTwo;
    }
 
-  }
-    
   
+}
+    
+
+  */
+
+// A class to represent a calculator
+class Calculator {
+  // A constructor to initialize the display and total properties
+  constructor() {
+    this.display = '';
+    this.total = [];
+  }
+
+  // A method to append a digit to the display and total
+  appendDigit(digit) {
+    this.display = this.display + digit;
+    document.querySelector('#theResults').innerText = this.display;
+    this.total.push(digit);
+  }
+
+  // A method to append an operator to the display and total
+  appendOperator(operator) {
+    this.display = this.display + operator;
+    document.querySelector('#theResults').innerText = this.display;
+    this.total.push(operator);
+  }
+
+  // A method to evaluate the total and display the result
+  evaluate() {
+    this.display = this.total;
+    let capStringOne = '';
+    let capStringTwo = '';
+
+    if (this.display.includes('-')) {
+      for (let i = 0; i <= this.display.length; i++) {
+        capStringOne = Number(
+          this.display.slice(0, this.display.indexOf('-')).join('')
+        );
+        capStringTwo = Number(
+          this.display.slice(this.display.indexOf('-') + 1).join('')
+        );
+      }
+      document.querySelector('#theResults').innerText =
+        capStringOne - capStringTwo;
+    }
+
+    if (this.display.includes('÷')) {
+      for (let i = 0; i <= this.display.length; i++) {
+        capStringOne = Number(
+          this.display.slice(0, this.display.indexOf('÷')).join('')
+        );
+        capStringTwo = Number(
+          this.display.slice(this.display.indexOf('÷') + 1).join('')
+        );
+      }
+      document.querySelector('#theResults').innerText =
+        capStringOne / capStringTwo;
+    }
+
+    if (this.display.includes('×')) {
+      for (let i = 0; i <= this.display.length; i++) {
+        capStringOne = Number(
+          this.display.slice(0, this.display.indexOf('×')).join('')
+        );
+        capStringTwo = Number(
+          this.display.slice(this.display.indexOf('×') + 1).join('')
+        );
+      }
+      document.querySelector('#theResults').innerText =
+        capStringOne * capStringTwo;
+    }
+
+    if (this.display.includes('+')) {
+      for (let i = 0; i <= this.display.length; i++) {
+        capStringOne = Number(
+          this.display.slice(0, this.display.indexOf('+')).join('')
+        );
+        capStringTwo = Number(
+          this.display.slice(this.display.indexOf('+') + 1).join('')
+        );
+      }
+      document.querySelector('#theResults').innerText =
+        capStringOne + capStringTwo;
+    }
+  }
+}
+
+// An instance of the Calculator class
+let calc = new Calculator();
+
+// Adding event listeners to the buttons
+document.querySelector('#seven').addEventListener('click', () => {
+  calc.appendDigit(7);
+});
+document.querySelector('#four').addEventListener('click', () => {
+  calc.appendDigit(4);
+});
+document.querySelector('#one').addEventListener('click', () => {
+  calc.appendDigit(1);
+});
+document.querySelector('#zero').addEventListener('click', () => {
+  calc.appendDigit(0);
+});
+document.querySelector('#eight').addEventListener('click', () => {
+  calc.appendDigit(8);
+});
+document.querySelector('#five').addEventListener('click', () => {
+  calc.appendDigit(5);
+});
+document.querySelector('#two').addEventListener('click', () => {
+  calc.appendDigit(2);
+});
+document.querySelector('#dotSign').addEventListener('click', () => {
+  calc.appendDigit('.');
+});
+document.querySelector('#nine').addEventListener('click', () => {
+  calc.appendDigit(9);
+});
+document.querySelector('#six').addEventListener('click', () => {
+  calc.appendDigit(6);
+});
+document.querySelector('#three').addEventListener('click', () => {
+  calc.appendDigit(3);
+});
+document.querySelector('#equalSign').addEventListener('click', () => {
+  calc.evaluate();
+});
+document.querySelector('#divideSign').addEventListener('click', () => {
+  calc.appendOperator('÷');
+});
+document.querySelector('#multiplySign').addEventListener('click', () => {
+  calc.appendOperator('×');
+});
+document.querySelector('#plusSign').addEventListener('click', () => {
+  calc.appendOperator('+');
+});
+document.querySelector('#minusSign').addEventListener('click', () => {
+  calc.appendOperator('-');
+});
   
 
 
